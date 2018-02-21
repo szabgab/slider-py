e0d2697c8293a5b85f90f38f1e090033a41ebaab
Gabor Szabo <gabor@szabgab.com>
2018-02-21 18:14:29 +0200

    function returns True

--- test_make_dir.py
+++ test_make_dir.py
@@ -4,4 +4,4 @@ def make_directory_test(tmpdir):
     root = str(tmpdir)
     target_dir = os.path.join(root, 'abcd')
     generate_html_files(self, target_dir)
-    assert os.path.exists(target_dir) == 1
+    assert os.path.exists(target_dir)
