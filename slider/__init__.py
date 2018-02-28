a882002744c22e97e264ba10ad60a1888a0b8fb2
Gabor Szabo <gabor@szabgab.com>
2018-02-28 22:49:27 +0200

    separate the code reading the command line arguments

--- slider/__init__.py
+++ slider/__init__.py
@@ -1,57 +1,12 @@
-import argparse
 import re
 import os
 import yaml
 import shutil
-import json
 from jinja2 import Environment, FileSystemLoader
 
 class SliderError(Exception):
     pass
 
-def main():
-    '''
-    Use cases:
-    --parse --md cases/all.md
-    --parse --md cases/all.md > cases/dom/all.json
-    --html  --md cases/all.md --dir cases/html/all
-    '''
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--parse", help="Create DOM of md file as JSON file", action='store_true')
-    parser.add_argument("--html", help="Create HTML files", action='store_true')
-    parser.add_argument("--md", help="Name of and md file")
-    parser.add_argument("--dir", help="Path to the HTML directory")
-    parser.add_argument("--templates", help="Directory of the HTML templates")
-    parser.add_argument("--static", help="Directory of the static files that will be copied to the html directory")
-
-    args = parser.parse_args()
-
-    if args.parse:
-        if not args.md:
-            print("--md was missing")
-            parser.print_help()
-            exit(1)
-        slider = Slider()
-        dom = slider.parse(args.md)
-        json_str = json.dumps(dom, sort_keys=True, indent=4, separators=(',', ': '), ensure_ascii=False)
-        print(json_str)
-        exit()
-
-    if args.html:
-        if not args.md:
-            print("--md was missing")
-            parser.print_help()
-            exit(1)
-        if not args.dir:
-            print("--dir was missing")
-            parser.print_help()
-            exit(1)
-        slider = Slider(templates = args.templates, static = args.static)
-        slider.parse(args.md)
-        slider.generate_html_files(args.dir)
-        exit()
-
-    parser.print_help()
 
 
 class Slider(object):
@@ -321,6 +276,3 @@ class Slider(object):
             for entry in os.listdir(self.static):
                 shutil.copy(os.path.join(self.static, entry), in_dir)
 
-
-if __name__ == '__main__':
-    main()
