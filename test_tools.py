7a08731a2bbb813f6754fa415d61ca5de4b842e1
Gabor Szabo <gabor@szabgab.com>
2018-09-21 22:00:28 +0300

    better error reporting

--- test_tools.py
+++ test_tools.py
@@ -3,9 +3,9 @@ import os
 
 def compare_dirs(left, right, name):
     dcmp = filecmp.dircmp(left, right)
-    assert dcmp.left_only == []  # some unexpected files were generated
-    assert dcmp.right_only == [] # some expected files were NOT generated
+    assert dcmp.left_only == [], 'some unexpected files were generated'
+    assert dcmp.right_only == [], 'some expected files were NOT generated'
     if dcmp.diff_files != []:
         for filename in dcmp.diff_files:
             print("diff {}/{} {}".format(left, filename, os.path.join('cases', 'html', name, filename)))
-    assert dcmp.diff_files == [] # the content of some files is different
+    assert dcmp.diff_files == [], 'the content of some files is different'
