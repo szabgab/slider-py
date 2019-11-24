94bbc7a7e524687246bdee0a0f83eaf3b0e09e6a
Gabor Szabo <gabor@szabgab.com>
2019-11-24 23:38:31 +0200

    more cli test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -19,3 +19,22 @@ def test_cli(tmpdir):
     assert out == b''
     assert err == b''
 
+def test_cli_empty():
+    cmd = [sys.executable, "slider.py"]
+    out, err, code = qx(cmd)
+    print(out)
+    print(err)
+    assert code == 0
+    assert 'usage: slider.py' in out.decode('utf-8')
+    assert err == b''
+
+def test_cli_empty_parse():
+    cmd = [sys.executable, "slider.py", '--parse']
+    out, err, code = qx(cmd)
+    print(out)
+    print(err)
+    assert code == 1
+    assert 'usage: slider.py' in out.decode('utf-8')
+    assert '--md was missing' in out.decode('utf-8')
+    assert err == b''
+
