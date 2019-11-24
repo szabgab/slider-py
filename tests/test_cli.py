871e52b60957bf6a521376e42c86afe3166e8080
Gabor Szabo <gabor@szabgab.com>
2019-11-24 23:34:47 +0200

    separate qx

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,17 +1,21 @@
 import sys
 import subprocess
 
-def test_cli(tmpdir):
-    temp_dir = str(tmpdir)
-    cmd = [sys.executable, "slider.py", "--md", "cases/all.md", "--html", "--dir",  temp_dir]
+def qx(cmd):
     proc = subprocess.Popen(cmd,
         stdout = subprocess.PIPE,
         stderr = subprocess.PIPE,
     )
     out,err = proc.communicate()
+    return out, err, proc.returncode
+
+def test_cli(tmpdir):
+    temp_dir = str(tmpdir)
+    cmd = [sys.executable, "slider.py", "--md", "cases/all.md", "--html", "--dir",  temp_dir]
+    out, err, code = qx(cmd)
     print(out)
     print(err)
-    assert proc.returncode == 0
+    assert code == 0
     assert out == b''
     assert err == b''
 
