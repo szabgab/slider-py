4262ed2aa30615e636f75558211a43d35f6a48f7
Gabor Szabo <gabor@szabgab.com>
2019-11-24 20:11:33 +0200

    check dom before checking html

--- tests/test_remote_dir.py
+++ tests/test_remote_dir.py
@@ -1,16 +1,24 @@
 import os
+import sys
+import json
 from tools import compare_dirs
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from slider import Slider, SliderError
 
 def test_other_dir(tmpdir):
     root = str(tmpdir)
     cwd = os.getcwd()
     os.chdir(root)
-    from slider import Slider, SliderError
     slider = Slider()
 
     target_dir = os.path.join(root, 'html')
     os.mkdir(target_dir)
     pages = slider.parse(os.path.join(cwd, 'cases', 'all.md'))
+    with open(os.path.join(cwd, 'cases/dom/all.json')) as fh:
+        expected = json.load(fh)
+    assert expected == pages
+
     slider.generate_html_files(target_dir)
     compare_dirs(target_dir, os.path.join(cwd, 'cases', 'html', 'all'), 'all')
 
