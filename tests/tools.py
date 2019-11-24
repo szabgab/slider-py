0265f095acc60ef61848177f48f12990293cee12
Gabor Szabo <gabor@szabgab.com>
2019-11-24 21:20:03 +0200

    check for the info.yaml files

--- tests/tools.py
+++ tests/tools.py
@@ -2,8 +2,11 @@ import filecmp
 import os
 
 def compare_dirs(left, right, name):
+    print("Left: {} Right: {} name: {}".format(left, right, name))
     dcmp = filecmp.dircmp(left, right)
-    assert dcmp.left_only == ['info.yaml'], 'some unexpected files were generated'
+    #assert 'info.yaml' in dcmp.left_only, "info.yaml missing in '{}' test case".format(name)
+    #assert dcmp.left_only == ['info.yaml'], 'some unexpected files were generated: ' + ', '.join(dcmp.left_only)
+    assert dcmp.left_only == [], 'some unexpected files were generated: ' + ', '.join(dcmp.left_only)
     assert dcmp.right_only == [], 'some expected files were NOT generated'
     if dcmp.diff_files != []:
         for filename in dcmp.diff_files:
