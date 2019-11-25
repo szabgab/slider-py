1b7191359160a30cc1d3726273ce10a05c9aaca6
Gabor Szabo <gabor@szabgab.com>
2019-11-25 08:41:47 +0200

    remove unused test file

--- tests/test_make_dir.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def make_directory_test(tmpdir):
-    root = str(tmpdir)
-    target_dir = os.path.join(root, 'abcd')
-    generate_html_files(self, target_dir)
-    assert os.path.exists(target_dir)
