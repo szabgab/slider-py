322320b344d873b867f8241bfc9add6470875065
Gabor Szabo <gabor@szabgab.com>
2019-11-25 21:20:28 +0200

    allow accessing the yaml file from a distance

--- tests/test_remote_dir.py
+++ tests/test_remote_dir.py
@@ -31,17 +31,24 @@ def test_other_dir(tmpdir):
         compare_dirs(target_dir, os.path.join(original, 'cases', 'html', 'all'), 'all')
 
 
-#def test_other_dir_multi(tmpdir):
-#    root = str(tmpdir)
-#    original = os.getcwd()
-#
-#    yml_file = os.path.join(original, 'cases', 'multi.yml')
-#
-#    with open(yml_file, 'r', encoding="utf-8") as fh:
-#        expected = yaml.load(fh, Loader=yaml.FullLoader)
-#
-#    with cwd(root):
-#        multi_slider = MultiSlider()
-#        data = multi_slider.process_yml(yml_file)
-#        assert data == expected
+def test_other_dir_multi(tmpdir):
+    root = str(tmpdir)
+    original = os.getcwd()
+
+    yml_file = os.path.join(original, 'cases', 'multi.yml')
+
+    with open(yml_file, 'r', encoding="utf-8") as fh:
+        expected = yaml.load(fh, Loader=yaml.FullLoader)
+
+    expected['pages'] = []
+    for name in ['chapter', 'all']:
+        js_file = os.path.join(original, "cases/dom/{}.json".format(name))
+        with open(js_file) as fh:
+            expected['pages'].append(json.load(fh))
+
+
+    with cwd(root):
+        multi_slider = MultiSlider()
+        data = multi_slider.process_yml(yml_file)
+        assert data == expected
 
